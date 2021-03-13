c7dd96d7dc3b08ff65ce1cea581f48b04fe01fae
Peter Benjamin <petermbenjamin@gmail.com>
2021-03-13 14:37:25 -0800

    feat: add night-owl

---
+++ themes/night-owl.js
@@ -0,0 +1,4 @@
+term_.prefs_.set("background-color", "#011627");
+term_.prefs_.set("foreground-color", "#d6deeb");
+term_.prefs_.set("cursor-color",     "#d6deeb");
+term_.prefs_.set("color-palette-overrides", ["#011627","#ef5350","#22da6e","#addb67","#82aaff","#c792ea","#21c7a8","#ffffff","#575656","#ef5350","#22da6e","#ffeb95","#82aaff","#c792ea","#7fdbca","#ffffff"]);
